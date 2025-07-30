93f52b98d21bd5c9e07599716fd6a62e7edc5047
yoshimidevz <yoshimidev1@gmail.com>
2025-07-30 14:48:34 -0300

    feat: adição de lógica de fechamento da escotilha a partir dos sensores

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,21 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
+//Sensor ultrassônico
+const int echoPin = 26;
+const int trigPin = 25;
+
+const int pistaoDois = 19; //pistão esquerda superior
+const int pistaoUm = 18; //pistão direita inferior
+
+BH1750 lightMeter(0x23);
+
+//BH1750 PINOUTS
 // VCC - PROTOBOARD: 08 PINOUT: 3.3V / LARANJA
 // GND - PROTOBOARD: 09 PINOUT: GND / PRETO
 // SCL - PROTOBOARD: 10 PINOUT: D22 / AMARELO
 // SDA - PROTOBOARD: 11 PINOUT: D21 / VERDE
-// ADDR - PROTOBOARD: 12 PINOUT: GND ou desconectado 
-
-// Definição dos pinos do sensor ultrassônico
-const int echoPin = 26;
-const int trigPin = 25;
-
-// Definição dos pinos dos pistões
-const int pistLeftSup = 19; //pistão esquerda superior
-const int pistLeftInf = 20; //pistão esquerda inferior
-const int pistRigSup = 18; //pistão direita superior
-const int pistRigInf = 17; //pistão direita inferior
-
-BH1750 lightMeter(0x23);
+// ADDR - PROTOBOARD: 12 PINOUT: GND ou desconectadoQ / AZUL
 
 float readDistance() {
   digitalWrite(trigPin, LOW);
@@ -48,10 +46,8 @@ void setup() {
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
 
-  pinMode(pistLeftSup, OUTPUT);
-  pinMode(pistLeftInf, OUTPUT);
-  pinMode(pistRigSup, OUTPUT);
-  pinMode(pistRigInf, OUTPUT);
+  pinMode(pistaoDois, OUTPUT);
+  pinMode(pistaoUm, OUTPUT);
 
   if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
     Serial.println(F("BH1750 Advanced begin"));
@@ -66,11 +62,23 @@ void loop() {
   Serial.print(distance);
   Serial.println(" cm");
   delay(2000);
+
+
   if (lightMeter.measurementReady()) {
     float lux = lightMeter.readLightLevel();
     Serial.print("Light: ");
     Serial.print(lux);
     Serial.println(" lx");
+
+    if(lux > 100 && distance > 0 && distance < 10) {
+      Serial.println("Acionado.");
+      digitalWrite(pistaoDois, HIGH);
+      digitalWrite(pistaoUm, HIGH);
+    } else {
+      Serial.println("Desligado.");
+      digitalWrite(pistaoDois, LOW);
+      digitalWrite(pistaoUm, LOW);
+    }
   }
   delay(2000);
 }
