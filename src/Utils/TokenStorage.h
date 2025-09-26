c3190d6e139df361599414654eeef2b0e93c756a
yoshimidevz <yoshimidev1@gmail.com>
2025-09-26 11:24:09 -0300

    feat: implement token storage functions for API authentication

--- src/Utils/TokenStorage.h
+++ src/Utils/TokenStorage.h
@@ -1,16 +1,6 @@
+#pragma once
+#include <Arduino.h>
 #include <Preferences.h>
 
-Preferences prefs;
-
-void saveTokenToFlash(const String &token) {
-  prefs.begin("escotilha", false); 
-  prefs.putString("api_token", token);
-  prefs.end();
-}
-
-String readTokenFromFlash() {
-  prefs.begin("escotilha", true); 
-  String t = prefs.getString("api_token", "");
-  prefs.end();
-  return t;
-}
+void saveTokenToFlash(const String &token);
+String readTokenFromFlash();
